932be769cff8e98a3ed7718fcf84e0021d222da9
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-02-19 11:01:12 +0530

    ref is added

---
+++ server/models/Item.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  isInStock: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  restaurant:{
+    restaurantId: {
+      type: ObjectId,
+      required: true,
+      ref: 'Restaurant'
+    },
+    outlet: {
+      type: ObjectId,
+      required: true,
+      ref: 'Outlet'
+    }
+  },
+  addedOn: {
+    type: Date,
+    default: Date.now
+  }
+}) 
+
+module.exports = mongoose.model('Item', itemSchema)
