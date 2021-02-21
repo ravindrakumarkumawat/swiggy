dec65250c8cf1a121c238b8cfabbabe457a6e9cc
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-02-21 14:13:01 +0530

    engine is added and serving directory is added and route is created

--- server/server.js
+++ server/server.js
@@ -6,10 +6,16 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+
 app.use(cors())
 
 app.get("/swiggy/api", (req, res) => {
   res.json({message: 'This is swiggy backend'})
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 })
 
 app.listen(PORT, () => {
