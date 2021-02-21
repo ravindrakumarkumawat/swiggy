18c890544929ef7f4a99e482e1851f24d5a3daa9
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-02-21 12:54:18 +0530

    cors is installed

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
+
+app.use(cors())
 
 app.get("/swiggy/api", (req, res) => {
   res.json({message: 'This is swiggy backend'})
