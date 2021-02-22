5d5d3f9c9edb73ab85584851b8682c5f948b51a9
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-02-22 13:18:58 +0530

    router is added to server for restaurants

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express')
 const cors = require('cors')
 
+const restaurantRouter = require('./routes/restaurant')
+
 const app = express()
 
 const PORT = process.env.PORT || 3001
@@ -9,6 +11,8 @@ const PORT = process.env.PORT || 3001
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 app.use(cors())
+
+app.use("/api/restaurant", restaurantRouter)
 
 app.get("/api", (req, res) => {
   res.json({message: 'This is swiggy backend'})
