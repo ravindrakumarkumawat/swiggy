061a67408ea2d26635a71f244f2ea28352fafbf2
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-02-22 14:34:26 +0530

    delete controller is created for restaurant

--- server/controllers/restaurant.js
+++ server/controllers/restaurant.js
@@ -18,13 +18,29 @@ const getRestaurant = async (req, res) => {
     const restaurant = await Restaurant.findOne({ _id: id })
 
     if(!restaurant) {
-      res.status(404).json({ message: 'Restaurant not found' })
+      return res.status(404).json({ error: 'Restaurant not found' })
     }
 
     res.status(200).json(restaurant)
 
   } catch (err) {
     res.status(500).json({error: err.message})
+  }
+}
+
+const deleteRestaurant = (req, res) => {
+  try {
+    const { id } = req.params
+
+    const del = await User.findOneAndDelete({ _id: id })
+
+    if (!del) {
+      return res.status(404).json({ error: "Restaurant not found" })
+    }
+    
+    res.status(200).json({ deleted: true })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
   }
 }
 
