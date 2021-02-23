c876b496f74497eb21f4679cfa19022417ab08e8
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-02-23 17:53:35 +0530

    included the db and run it and install the older version of mongoose

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const path = require('path')
 const express = require('express')
 const cors = require('cors')
+const { connectDB } = require('./db/connectDb')
 
 const restaurantRouter = require('./routes/restaurant')
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
-
+connectDB()
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 
+app.use(express.json())
 app.use(cors())
 
 app.use("/api/restaurant", restaurantRouter)
