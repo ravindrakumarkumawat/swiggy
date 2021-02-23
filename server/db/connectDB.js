83d015d4e11a14d28f229bb2c5761bf47359c1f3
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-02-23 17:52:39 +0530

    URI is not fetching from env file

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,11 +1,11 @@
-require("dotenv").config()
+// require("dotenv").config()
 const mongoose = require("mongoose")
 
-const URI = process.env.MONGODB_CONNECTION_STRING
+// const URI = process.env.MONGODB_CONNECTION_STRING
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(URI, {
+    await mongoose.connect('mongodb+srv://swiggy:ppF3mYVDhdAMEua4@cluster0.ay866.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
