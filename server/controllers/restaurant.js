39ea67981803f2bc1186884403cb1cf8b522cd03
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-02-24 14:29:09 +0530

    delete item schema is added

--- server/controllers/restaurant.js
+++ server/controllers/restaurant.js
@@ -197,7 +197,15 @@ const updateItem = async (req, res) => {
 
 const deleteItem = async (req, res) => {
   try {
-    res.status(200).json({message: 'delete item'})
+    const { id, itemId } = req.params
+
+    const del = await Item.findOneAndDelete({ _id: itemId, restaurantId: id })
+
+    if (!del) {
+      return res.status(404).json({ error: "Item not found" })
+    }
+    
+    res.status(200).json({ deleted: true })
 
   } catch (err) {
     res.status(500).json({error: err.message})
