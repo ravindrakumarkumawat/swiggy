e2bef7a6a48c8ec88ebac79797c73d8cea785b83
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-05 13:19:02 +0530

    authenticationToken middleware is removed

--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -1,19 +1,19 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
+// function authenticateToken(req, res, next) {
+//   const authHeader = req.headers['authorization']
+//   const token = authHeader && authHeader.split(' ')[1]
+//   if (token == null) return res.sendStatus(401)
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, id) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.id = id
-    next()
-  })
-}
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, id) => {
+//     console.log(err)
+//     if (err) return res.sendStatus(403)
+//     req.id = id
+//     next()
+//   })
+// }
 
 
-module.exports = { authenticateToken }
+// module.exports = { authenticateToken }
--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const { authenticateToken } = require('../middlewares/authenticateToken')
 
 const {
   getAllRestaurants,
