f576e32d1930e7d2d0c2d90b4143034dd65e11cf
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-08 13:03:53 +0530

    did cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
 import React, {useState, useEffect} from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("/api")
+    fetch("http://localhost:3001/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 }
