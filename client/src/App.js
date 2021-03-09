1b5f3b64d8932ee63534ffcc0d2c635799154859
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-09 10:45:37 +0530

    route is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
 
   return (
-    <div className="App">
-      <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-        <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="/img/erin-lindford.jpg" alt="Woman's Face"/>
-        <div className="text-center space-y-2 sm:text-left">
-          <div className="space-y-0.5">
-            <p className="text-lg text-black font-semibold">
-              Erin Lindford
-            </p>
-            <p className="text-gray-500 font-medium">
-              Product Engineer
-            </p>
-          </div>
-          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
-        </div>
-      </div>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
