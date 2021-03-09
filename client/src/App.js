261dc85c068d732d0f98a36ec0145ff537758f61
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-09 13:33:37 +0530

    various route is created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect } from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
+import Dropdown from './components/Dropdown';
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
 
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if(window.innerWidth > 768 && isOpen) {
+        setIsOpen(false)
+      }
+    }
+
+    window.addEventListener('resize', hideMenu) 
+
+    return () => {
+      window.removeEventListener('resize', hideMenu)
+    }
+
+  })
   return (
     <>
-      <Navbar />
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
+      <Switch>
+        <Route exact path="/"><Redirect to="/restaurants" /></Route>
+        <Route path="/restaurants"><h1>Home Page</h1></Route>
+        <Route path="/search"><h1>Search</h1></Route>
+        <Route path="/offers/restaurant"><h1>Offers</h1></Route>
+        <Route path="/support"><h1>Help</h1></Route>
+        <Route path="/my-account"><h1>My Account</h1></Route>
+        <Route path="/checkout"><h1>Checkout</h1></Route>
+      </Switch>
     </>
   );
 }
