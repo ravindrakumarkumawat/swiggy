b94898104a3bc045610e0dc28773a20b800f2eea
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-09 17:34:41 +0530

    Creating the side bar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,59 @@
-import React from 'react'
-import Image1 from "../images/image1.jpg"
+import React from "react";
+import Image1 from "../images/image1.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <div className="flex flex-row justify-evenly bg-yellow-300 h-85 px-14 max-h-1200 min-h-1200">
-        <img src={Image1} className="h-72 w-72 p-7" alt="image1"/>
-        <img src={Image1} className="h-72 w-72 p-7" alt="image1"/>
-        <img src={Image1} className="h-72 w-72 p-7" alt="image1"/>
-        <img src={Image1} className="h-72 w-72 p-7" alt="image1"/>
+    <>
+      <div className="flex flex-row justify-evenly bg-yellow-300 h-85 max-h-1200 min-h-1200">
+        <img src={Image1} className="h-64 w-64 m-6" alt="image1" />
+        <img src={Image1} className="h-64 w-64 m-6" alt="image1" />
+        <img src={Image1} className="h-64 w-64 m-6" alt="image1" />
+        <img src={Image1} className="h-64 w-64 m-6" alt="image1" />
       </div>
-    </div>
-  )
-}
+      <div className="grid grid-cols-2">
+        <div className="bg-white border-2 w-64 shadow-md mx-12">
+          <div>
+            <div>Icons</div>
+            <div>
+              <h1>Top Picks</h1>
+              <p>122 OPTIONS</p>
+            </div>
+          </div>
+          <div>
+            <div>Icons</div>
+            <div>
+              <h1>Premium</h1>
+              <p>122 OPTIONS</p>
+            </div>
+          </div>
+          <div>
+            <div>Icons</div>
+            <div>
+              <h1>Only On Swiggy</h1>
+              <p>122 OPTIONS</p>
+            </div>
+          </div>
+          <div>
+            <div>Icons</div>
+            <div>
+              <h1>Vegetarian Options</h1>
+              <p>122 OPTIONS</p>
+            </div>
+          </div>
+          <div>
+            <div>Icons</div>
+            <div>
+              <h1>See All</h1>
+              <p>122 OPTIONS</p>
+            </div>
+          </div>
+        </div>
+        <div>
+        Something to load
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
