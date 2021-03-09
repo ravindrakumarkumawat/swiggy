c39ceaec3ad1053623643e60e9634e4a73fb4dd6
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-09 13:32:22 +0530

    toggle of the icon is added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <nav className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono" role="navigation">
       <Link to="/" className="pl-8">
-        Navbar
+        logo
       </Link>
-      <div className="px-4 cursor-pointer md:hidden">
+      <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
         <svg
           className="w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
@@ -24,10 +24,11 @@ const Navbar = () => {
         </svg>
       </div>
       <div className="pr-8 md:block hidden">
-        <Link to="/" className="p-4">Home</Link>
-        <Link to="/menu" className="p-4">Menu</Link>
-        <Link to="/about" className="p-4">About</Link>
-        <Link to="/contact" className="p-4">Contact</Link>
+        <Link to="/search" className="p-4">Search</Link>
+        <Link to="/offers/restaurant" className="p-4">Offers</Link>
+        <Link to="/support" className="p-4">Help</Link>
+        <Link to="/my-account" className="p-4">Ravindra</Link>
+        <Link to="/checkout" className="p-4">Cart</Link>
       </div>
     </nav>
   );
