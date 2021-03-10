cd9350b1967510f68517dc5edb9d498fa0299817
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-10 18:15:58 +0530

    box sticky applied

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -31,7 +31,7 @@ const Home = () => {
         />
       </div>
       <div className="flex">
-        <div className="bg-white border-2 w-64 h-4/6 shadow-md mx-12 py-7 cursor-pointer">
+        <div className="bg-white border-2 w-64 h-4/6 shadow-md mx-12 py-7 cursor-pointer sticky top-0">
           <div className="flex flex-row p-3 hover:bg-yellow-500 text-yellow-500 hover:text-white">
             <div>
               <svg
@@ -130,6 +130,94 @@ const Home = () => {
           </div>
         </div>
         <div className="mx-5 my-16 border-b-2 border-dashed">
+          <div className="">
+            <div className="font-semibold text-3xl text-gray-900">
+              Top Picks
+            </div>
+            <div className="grid grid-cols-3 py-12 gap-8">
+              <div className="w-72 bg-white cursor-pointer border-2 border-white hover:border-gray-200 hover:shadow-md p-3.5">
+                <div className="w-64 h-40">
+                  <img src={Food} alt="food1" />
+                </div>
+                <div className="my-3">
+                  <div className="text-gray-700 font-bold">Lunch Box</div>
+                  <div className="text-gray-500 font-md text-xs">North Indian, Desserts, Biryani</div>
+                </div>
+                <div className="flex justify-between my-3 text-gray-500 font-md text-xs">
+                  <div className="bg-green-500 text-white px-2">
+                    <span className="">*</span>
+                    <span>4.2</span>
+                  </div>
+                  <div>•</div>
+                  <div>29 MINS</div>
+                  <div>•</div>
+                  <div className="">₹200 FOR TWO</div>
+                </div>
+                <div className="border-t-2 text-sm text-red-700 py-2">
+                  <span className=""></span>
+                  <span className="">50% off | Use SWIGGYIT</span>
+                </div>
+                <div className="text-sm text-red-700 py-0">
+                  <span className=""></span>
+                  <span className="">Flat ₹30 Paytm Cashback</span>
+                </div>
+              </div>
+              <div className="w-72 bg-white cursor-pointer border-2 border-white hover:border-gray-200 hover:shadow-md p-3.5">
+                <div className="w-64 h-40">
+                  <img src={Food} alt="food1" />
+                </div>
+                <div className="my-3">
+                  <div className="text-gray-700 font-bold">Lunch Box</div>
+                  <div className="text-gray-500 font-md text-xs">North Indian, Desserts, Biryani</div>
+                </div>
+                <div className="flex justify-between my-3 text-gray-500 font-md text-xs">
+                  <div className="bg-green-500 text-white px-2">
+                    <span className="">*</span>
+                    <span>4.2</span>
+                  </div>
+                  <div>•</div>
+                  <div>29 MINS</div>
+                  <div>•</div>
+                  <div className="">₹200 FOR TWO</div>
+                </div>
+                <div className="border-t-2 text-sm text-red-700 py-2">
+                  <span className=""></span>
+                  <span className="">50% off | Use SWIGGYIT</span>
+                </div>
+                <div className="text-sm text-red-700 py-0">
+                  <span className=""></span>
+                  <span className="">Flat ₹30 Paytm Cashback</span>
+                </div>
+              </div>
+              <div className="w-72 bg-white cursor-pointer border-2 border-white hover:border-gray-200 hover:shadow-md p-3.5">
+                <div className="w-64 h-40">
+                  <img src={Food} alt="food1" />
+                </div>
+                <div className="my-3">
+                  <div className="text-gray-700 font-bold">Lunch Box</div>
+                  <div className="text-gray-500 font-md text-xs">North Indian, Desserts, Biryani</div>
+                </div>
+                <div className="flex justify-between my-3 text-gray-500 font-md text-xs">
+                  <div className="bg-green-500 text-white px-2">
+                    <span className="">*</span>
+                    <span>4.2</span>
+                  </div>
+                  <div>•</div>
+                  <div>29 MINS</div>
+                  <div>•</div>
+                  <div className="">₹200 FOR TWO</div>
+                </div>
+                <div className="border-t-2 text-sm text-red-700 py-2">
+                  <span className=""></span>
+                  <span className="">50% off | Use SWIGGYIT</span>
+                </div>
+                <div className="text-sm text-red-700 py-0">
+                  <span className=""></span>
+                  <span className="">Flat ₹30 Paytm Cashback</span>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="">
             <div className="font-semibold text-3xl text-gray-900">
               Top Picks
