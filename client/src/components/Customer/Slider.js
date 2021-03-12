f23aff585be23928c66fe20a142652f54dd21441
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-12 11:12:59 +0530

    images reduced and put inside array for slider

--- client/src/components/Customer/Slider.js
+++ client/src/components/Customer/Slider.js
@@ -1,34 +1,18 @@
-import React from 'react'
+import React from "react";
 import Image1 from "../../images/image1.jpg";
 import Image2 from "../../images/image2.jpg";
 import Image3 from "../../images/image3.jpg";
 import Image4 from "../../images/image4.jpg";
 
 const Slider = () => {
+  const images = [Image1, Image2, Image3, Image4];
   return (
     <div className="flex flex-row justify-evenly bg-yellow-300 h-85 max-h-1200 min-h-1200">
-    <img
-      src={Image1}
-      className="h-64 w-64 m-6 cursor-pointer"
-      alt="image1"
-    />
-    <img
-      src={Image2}
-      className="h-64 w-64 m-6 cursor-pointer"
-      alt="image2"
-    />
-    <img
-      src={Image3}
-      className="h-64 w-64 m-6 cursor-pointer"
-      alt="image3"
-    />
-    <img
-      src={Image4}
-      className="h-64 w-64 m-6 cursor-pointer"
-      alt="image4"
-    />
-  </div>
-  )
-}
+      {
+        images.map((image) => <img src={image} className="h-64 w-64 m-6 cursor-pointer" alt="some description" />)
+      }
+    </div>
+  );
+};
 
-export default Slider
+export default Slider;
