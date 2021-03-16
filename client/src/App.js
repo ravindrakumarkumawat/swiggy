5c511938a574dca29d1e1168f5e49f394fe71bc1
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-16 09:30:42 +0530

    restaurant partner sidebar is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect } from 'react'
-import './App.css';
-import Navbar from './components/Customer/Navbar';
-import Dropdown from './components/Customer/Dropdown';
-import {Switch, Route, Redirect} from 'react-router-dom'
-import Home from './pages/Home';
-import RestaurantNavbar from './components/RestaurantPartner/Navbar'
+import React, { useState, useEffect } from "react"
+import "./App.css"
+import Navbar from "./components/Customer/Navbar"
+import Dropdown from "./components/Customer/Dropdown"
+import { Switch, Route, Redirect } from "react-router-dom"
+import Home from "./pages/Home"
+import RestaurantNavbar from "./components/RestaurantPartner/Navbar"
+import Sidebar from "./components/RestaurantPartner/Sidebar"
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
@@ -15,17 +16,16 @@ function App() {
 
   useEffect(() => {
     const hideMenu = () => {
-      if(window.innerWidth > 768 && isOpen) {
+      if (window.innerWidth > 768 && isOpen) {
         setIsOpen(false)
       }
     }
 
-    window.addEventListener('resize', hideMenu) 
+    window.addEventListener("resize", hideMenu)
 
     return () => {
-      window.removeEventListener('resize', hideMenu)
+      window.removeEventListener("resize", hideMenu)
     }
-
   })
   return (
     <>
@@ -40,15 +40,39 @@ function App() {
         <Route path="/my-account"><h1>My Account</h1></Route>
         <Route path="/checkout"><h1>Checkout</h1></Route>
   </Switch>*/}
-        
-    <RestaurantNavbar />
-    <Switch>
-      <Route path="/restaurant-partner">
-        <div>Home of restaurant</div>
-      </Route>
-    </Switch>
+
+      <RestaurantNavbar />
+      <div className="flex">
+        <Sidebar />
+        <Switch>
+          <Route exact path="/restaurant-partner">
+            <div>Home nnnnn</div>
+          </Route>
+          <Route path="/restaurant-partner/orders">
+            <div>Order</div>
+          </Route>
+          <Route path="/restaurant-partner/menus">
+            <div>Menu</div>
+          </Route>
+          <Route path="/restaurant-partner/reviews">
+            <div>Review</div>
+          </Route>
+          <Route path="/restaurant-partner/settings">
+            <div>Settings</div>
+          </Route>
+          <Route path="/restaurant-partner/payments">
+            <div>Payments</div>
+          </Route>
+          <Route path="/restaurant-partner/accounts">
+            <div>Accounts</div>
+          </Route>
+          <Route path="/restaurant-partner/support">
+            <div>Help</div>
+          </Route>
+        </Switch>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/RestaurantPartner/Sidebar.js
@@ -0,0 +1,175 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const menuData = [
+  {
+    title: "Home",
+    link: "/restaurant-partner",
+    icon: (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+      </svg>
+    ),
+  },
+  {
+    title: "Order",
+    link: "/restaurant-partner/orders",
+    icon: (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM6 6.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+      </svg>
+    ),
+  },
+  {
+    title: "Menu",
+    link: "/restaurant-partner/menus",
+    icon: (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+        <path
+          fillRule="evenodd"
+          d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Review",
+    link: "/restaurant-partner/reviews",
+    icon: (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+]
+
+const otherData = [
+  {
+    title: "Setting",
+    link: "/restaurant-partner/settings",
+    icon: (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Payment",
+    link: "/restaurant-partner/payments",
+    icon: (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
+        <path
+          fillRule="evenodd"
+          d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Account",
+    link: "/restaurant-partner/accounts",
+    icon: (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Help",
+    link: "/restaurant-partner/support",
+    icon: (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+]
+
+const Sidebar = () => {
+  return (
+    <div className="w-64 h-screen bg-red-500">
+      <div>Image</div>
+      <div className="">
+        <div>MENU</div>
+        {menuData.map((data) => (
+          <Link className="flex items-center" key={data.title} to={data.link}>
+            <div>{data.icon}</div>
+            <div>{data.title}</div>
+          </Link>
+        ))}
+      </div>
+      <div>
+        <div>OTHERS</div>
+
+        {otherData.map((data) => (
+          <Link className="flex items-center" key={data.title} to={data.link}>
+            <div>{data.icon}</div>
+            <div>{data.title}</div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
